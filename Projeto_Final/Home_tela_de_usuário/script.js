0b5b993d9e95a8522e9f15fe8adbf570d84dadbd
IcaroFilipeD2 <103685187+IcaroFilipeD2@users.noreply.github.com>
2022-07-01 16:48:28 -0300

    Criação dos cards de notificações

--- Projeto_Final/Home_tela_de_usuário/script.js
+++ Projeto_Final/Home_tela_de_usuário/script.js
@@ -3,12 +3,12 @@
 let list = document.querySelectorAll('.navigation li');
 function activeLink() {
     list.forEach((item) =>
-        item.classList.removemove(''));
+    item.classList.removemove(''));
     this.classList.add('hovered');
 }
 
 list.forEach((item) =>
-    item.addEventListener('mouseover', activeLink));
+item.addEventListener('mouseover', activeLink));
 
 // MenuToggle 
 let toggle = document.querySelector('.toggle');
@@ -17,7 +17,7 @@ let main = document.querySelector('.main');
 let container = document.querySelector('.container');
 let conteudo = document.querySelector('.conteudo');
 
-toggle.onclick = function () {
+toggle.onclick = function() {
     navigation.classList.toggle('active');
     main.classList.toggle('active');
     toggle.classList.toggle('active');
